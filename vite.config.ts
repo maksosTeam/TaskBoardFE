c08f9d714eee249884d333b13710c9c7b4b76aea
jewe1V <zensot1@yandex.ru>
2026-05-17 12:37:30 +0500

    push for ci/cd4

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
 
 
-  const API = "https://project-domain.ru";
+  const API = "gateway-service:5000";
 
   return {
     plugins: [react()],
